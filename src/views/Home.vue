a7a20ffdca2c6b5ac1b28c93e29e33fe9f48e2e7
Sviatoslav <sviatoslavpanevnyk@gmail.com>
2021-09-01 16:11:38 +0300

    Setup client side and modificate .gitignore file

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({})
+export default class Home extends Vue {}
+</script>
